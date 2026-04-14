b47908d3d9da628891e6877f659d5176ad3bfd11
Arvid <arvid.alund@gmail.com>
2026-04-14 21:41:58 +0200

    Schema: uppdated albumSchema to have sharedWith

--- backend/models/Album.js
+++ backend/models/Album.js
@@ -14,6 +14,7 @@ const albumSchema = new mongoose.Schema({
     type: Date,
     required: true,
   },
+  sharedWith: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }]
 });
 
 albumSchema.index({ artist: 1, title: 1 }, { unique: true });
